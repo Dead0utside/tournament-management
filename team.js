3a0e69bced7687d8ed28b2d4bc50e5ccf240721b
guadacastro <guadycasmar123@gmail.com>
2024-10-04 16:41:58 +0200

    add function

--- team.js
+++ team.js
@@ -3,5 +3,9 @@ class Team() {
     constructor(name) {
         this.name = name;
         this.teamScore = 0;
+        this.members = [];
+    }
+    registerPlayer() {
+       this.members = this.members.push(new Player)
     }
 }
